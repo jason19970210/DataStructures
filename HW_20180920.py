65e4f07228ed98301c1f9a2a61cb12ee88e00a21
jason19970210 <jason19970210@gmail.com>
2018-09-22 22:25:45 +0800

    New

--- HW_20180920.py
+++ HW_20180920.py
@@ -2,10 +2,10 @@ import time
 
 n = int(input("請輸入十進制正整數: "))
 b = int(input("base: "))
-convertString = "0123456789ABCDEFGHIJKLMNOP"
+convertString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-tStartD = time.time()
+#tStartD = time.time()
 
 
 def convD(n,b):
@@ -13,10 +13,20 @@ def convD(n,b):
     if n < b:
         return convertString[n]
     else:
-        return convD(n//b,b) + convertString[n%b]
-    
+        return convD(n//b,b) + convertString[n % b]
+
+def convH(n,b):
+
+    while currentn != 0:
+        return
+        
+        
+
+
+
 
 #str = 'x 的值为： ' + repr(x) + ',  y 的值为：' + repr(y) + '...'
-print(n,"(10) ->",convD(n,b),"(",b,")")
-tEndD = time.time()
-print ("Cost ",tEndD - tStartD," sec")
+print("遞迴轉換:  ",n,"(10) ->",convD(n,b),"(",b,")")
+print("迴圈轉換:  ",n,"(10) ->",convH(n,b),"(",b,")")
+#tEndD = time.time()
+#print ("Cost ",tEndD - tStartD," sec")
